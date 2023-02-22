e6d90f8b91dc497a740499e6b9e57c3e30123bd3
qiaozhijian <935514540@qq.com>
2023-02-22 14:33:15 +0800

    update

--- node/receive_pcl.cpp
+++ node/receive_pcl.cpp
@@ -3,9 +3,9 @@
 #include "glog/logging.h"
 #include "file_manager.hpp"
 #include <Eigen/Core>
-#include "global_defination/global_defination.h"
 #include "System.h"
 #include "config.h"
+#include <ros/package.h>
 
 using namespace std;
 
@@ -16,7 +16,7 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh("~");
 
     google::InitGoogleLogging(argv[0]);
-    FLAGS_log_dir = ros_package_template::WORK_SPACE_PATH + "/Log";
+    FLAGS_log_dir = ros::package::getPath("ros_package_template") + "/Log";
     FLAGS_alsologtostderr = true;
     FLAGS_colorlogtostderr = true;
     FLAGS_log_prefix = true;
