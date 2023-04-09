899cae715e43347a08f73c0c6e14fb31cdb7cb9e
qiaozhijian <935514540@qq.com>
2023-04-09 11:35:18 +0800

    update

--- include/eigen_utils.h
+++ include/eigen_utils.h
@@ -17,6 +17,14 @@ inline double random_normal(double std) {
     return number;
 }
 
+inline double random_uniform(double min, double max) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(min, max);
+    double number = dis(gen);
+    return number;
+}
+
 inline Eigen::Vector3d random_unit_vector() {
     Eigen::Vector3d v;
     v << random_normal(1.0), random_normal(1.0), random_normal(1.0);
@@ -38,12 +46,43 @@ inline Eigen::Matrix3d random_rotation_matrix() {
     return R;
 }
 
+inline Eigen::Vector3d random_translation_vector(double max) {
+    Eigen::Vector3d t;
+    t << random_uniform(-max, max), random_uniform(-max, max), random_uniform(-max, max);
+    return t;
+}
+
+inline Eigen::Matrix4d random_transformation_matrix(double max) {
+    Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
+    T.block<3, 3>(0, 0) = random_rotation_matrix();
+    T.block<3, 1>(0, 3) = random_translation_vector(max);
+    return T;
+}
+
+inline cv::Point3f transform_point(cv::Point3f &p, Eigen::Matrix4d &T) {
+    Eigen::Vector4d p_eigen;
+    p_eigen << p.x, p.y, p.z, 1;
+    Eigen::Vector4d p_eigen_new = T * p_eigen;
+    cv::Point3f p_new;
+    p_new.x = p_eigen_new(0);
+    p_new.y = p_eigen_new(1);
+    p_new.z = p_eigen_new(2);
+    return p_new;
+}
+
 inline Eigen::Matrix<double, 3, 3> cv2eigen(const cv::Mat &cv_mat) {
     Eigen::Matrix<double, 3, 3> eigen_mat = Eigen::Matrix<double, 3, 3>::Identity();
     eigen_mat << cv_mat.at<double>(0, 0), cv_mat.at<double>(0, 1), cv_mat.at<double>(0, 2),
             cv_mat.at<double>(1, 0), cv_mat.at<double>(1, 1), cv_mat.at<double>(1, 2),
             cv_mat.at<double>(2, 0), cv_mat.at<double>(2, 1), cv_mat.at<double>(2, 2);
     return eigen_mat;
+}
+
+template<typename T>
+Eigen::Vector3d RotationVector(Eigen::Matrix<T, 3, 3> &R) {
+    Eigen::AngleAxis<T> angle_axis(R);
+    Eigen::Vector3d rotation_vector = angle_axis.angle() * angle_axis.axis();
+    return rotation_vector;
 }
 
 template<typename T>
